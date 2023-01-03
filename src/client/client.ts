22331c7b4c185e788b07a3fb0a77c9608fe26e68
Blessing <bntoko@gmail.com>
2023-01-03 19:20:09 +0200

    looking at material

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,79 +1,105 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import Stats from 'three/examples/jsm/libs/stats.module';
-import { GUI } from 'dat.gui';
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import Stats from "three/examples/jsm/libs/stats.module";
+import { GUI } from "dat.gui";
 
 // a scene is a tree like structure of Meshes, Lights, Groups, 3D Positions, Cameras(optional)
 const scene = new THREE.Scene();
-scene.add(new THREE.AxesHelper(1));
+scene.add(new THREE.AxesHelper(5));
 // a camera describes the view boundaries of the scene within the Frustum dimensions
 const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    .1,
-    1000
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
 );
-camera.position.z = 2;
+camera.position.z = 3;
 
 // Renderer takes all the data from the scene and camera and puts it on as HTML canvas
 const renderer = new THREE.WebGLRenderer({
-    antialias: true
+  antialias: true,
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
-// controls.addEventListener('change', render);
 
-const geometry = new THREE.BoxGeometry();
+const boxGeometry = new THREE.BoxGeometry();
+const sphereGeometry = new THREE.SphereGeometry();
+const icosahedronGeometry = new THREE.IcosahedronGeometry(1, 0);
+const planeGeometry = new THREE.PlaneGeometry();
+const torusKnotGeometry = new THREE.TorusKnotGeometry();
+
 const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true
+  color: 0x00ff00,
+//   wireframe: false,
+  wireframe: true,
 });
-const cube = new THREE.Mesh(geometry, material);
+// const material = new THREE.MeshNormalMaterial();
+
+const cube = new THREE.Mesh(boxGeometry, material);
+cube.position.x = 5;
 scene.add(cube);
 
-window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    render();
+const sphere = new THREE.Mesh(sphereGeometry, material);
+sphere.position.x = 3;
+scene.add(sphere);
+
+const icosahedron = new THREE.Mesh(icosahedronGeometry, material);
+icosahedron.position.x = 0;
+scene.add(icosahedron);
+
+const plane = new THREE.Mesh(planeGeometry, material);
+plane.position.x = -2;
+scene.add(plane);
+
+const torusKnot = new THREE.Mesh(torusKnotGeometry, material);
+torusKnot.position.x = -5;
+scene.add(torusKnot);
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  render();
 });
 
 const stats = Stats();
 document.body.appendChild(stats.dom);
 
+// const options = {
+//     side: {
+//         "FrontSide": THREE.FrontSide,
+//         "BackSide": THREE.BackSide,
+//         "DoubleSide": THREE.DoubleSide,
+//     }
+// }
+
 const gui = new GUI();
-const cubeFolder = gui.addFolder("Cube");
-const cubeRotationFolder = gui.addFolder("Rotation");
-const cubePositionFolder = gui.addFolder("Position");
-const cubeScaleFolder = gui.addFolder("Scale");
-cubeFolder.add(cube, "visible");
-cubeRotationFolder.add(cube.rotation, "x", 0, Math.PI * 2);
-cubeRotationFolder.add(cube.rotation, "y", 0, Math.PI * 2);
-cubeRotationFolder.add(cube.rotation, "z", 0, Math.PI * 2);
-cubePositionFolder.add(cube.position, "x", -10, 10, 2);
-cubePositionFolder.add(cube.position, "y", -10, 10, 2);
-cubePositionFolder.add(cube.position, "z", -10, 10, 2);
-cubeScaleFolder.add(cube.scale, "x", -5, 5, 2);
-cubeScaleFolder.add(cube.scale, "y", -5, 5, 2);
-cubeScaleFolder.add(cube.scale, "z", -5, 5, 2);
-cubeFolder.open();
-cubeRotationFolder.open();
-cubePositionFolder.open();
-cubeScaleFolder.open();
+const materialFolder = gui.addFolder("THREE.Material");
+// materialFolder.add(material, "transparent").onChange(() => material.needsUpdate = true);
+// materialFolder.add(material, "opacity", 0, 1, .01);
+// materialFolder.add(material, "depthTest");
+// materialFolder.add(material, "depthWrite");
+// materialFolder.add(material, "alphaTest", 0, 1, .01).onChange(() => updateMaterial());
+// materialFolder.add(material, "visible");
+// materialFolder.add(material, "side", options.side).onChange(() => updateMaterial());
+materialFolder.open();
+
+function updateMaterial() {
+    material.side = Number(material.side);
+    material.needsUpdate = true;
+}
 
 function animate() {
-    // cube.rotation.x += .02;
-    // cube.rotation.y += .02;
-
-    render();
-    requestAnimationFrame(animate);
-    stats.update();
+  //   controls.update();
+  render();
+  stats.update();
+  requestAnimationFrame(animate);
 }
 
 function render() {
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 }
 
 animate();
