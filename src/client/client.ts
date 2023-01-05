d721ae5411241fd013e564bc48cfe9766a3037fc
Blessing <bntoko@gmail.com>
2023-01-05 11:18:03 +0200

    added images; working on MeshBasicMaterials;

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,106 +1,128 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import Stats from "three/examples/jsm/libs/stats.module";
-import { GUI } from "dat.gui";
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import { GUI } from 'dat.gui'
 
-// a scene is a tree like structure of Meshes, Lights, Groups, 3D Positions, Cameras(optional)
-const scene: THREE.Scene = new THREE.Scene();
-scene.add(new THREE.AxesHelper(5));
-// a camera describes the view boundaries of the scene within the Frustum dimensions
-const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-camera.position.z = 3;
+const scene = new THREE.Scene()
+scene.add(new THREE.AxesHelper(5))
 
-// Renderer takes all the data from the scene and camera and puts it on as HTML canvas
-const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({
-  antialias: true,
-});
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+)
+camera.position.z = 3
 
-const controls: OrbitControls = new OrbitControls(camera, renderer.domElement);
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
-const boxGeometry: THREE.BoxGeometry = new THREE.BoxGeometry();
-const sphereGeometry: THREE.SphereGeometry = new THREE.SphereGeometry();
-const icosahedronGeometry: THREE.IcosahedronGeometry = new THREE.IcosahedronGeometry(1, 0);
-const planeGeometry: THREE.PlaneGeometry = new THREE.PlaneGeometry();
-const torusKnotGeometry: THREE.TorusKnotGeometry = new THREE.TorusKnotGeometry();
+new OrbitControls(camera, renderer.domElement)
 
-const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({
-  color: 0x00ff00,
-//   wireframe: false,
-  wireframe: true,
-});
-// const material: THREE.MeshBasicMaterial = new THREE.MeshNormalMaterial();
+const boxGeometry = new THREE.BoxGeometry()
+const sphereGeometry = new THREE.SphereGeometry()
+const icosahedronGeometry = new THREE.IcosahedronGeometry(1, 0)
+const planeGeometry = new THREE.PlaneGeometry()
+const torusKnotGeometry = new THREE.TorusKnotGeometry()
 
-const cube: THREE.Mesh = new THREE.Mesh(boxGeometry, material);
-cube.position.x = 5;
-scene.add(cube);
+const material = new THREE.MeshBasicMaterial() //{ color: 0x00ff00, wireframe: true })
 
-const sphere: THREE.Mesh = new THREE.Mesh(sphereGeometry, material);
-sphere.position.x = 3;
-scene.add(sphere);
+// const texture = new THREE.TextureLoader().load("img/grid.png")
+// material.map = texture
+const envTexture = new THREE.CubeTextureLoader().load(["img/px_50.png", "img/nx_50.png", "img/py_50.png", "img/ny_50.png", "img/pz_50.png", "img/nz_50.png"])
+// envTexture.mapping = THREE.CubeReflectionMapping
+envTexture.mapping = THREE.CubeRefractionMapping
+material.envMap = envTexture
 
-const icosahedron: THREE.Mesh = new THREE.Mesh(icosahedronGeometry, material);
-icosahedron.position.x = 0;
-scene.add(icosahedron);
+const cube = new THREE.Mesh(boxGeometry, material)
+cube.position.x = 5
+scene.add(cube)
 
-const plane: THREE.Mesh = new THREE.Mesh(planeGeometry, material);
-plane.position.x = -2;
-scene.add(plane);
+const sphere = new THREE.Mesh(sphereGeometry, material)
+sphere.position.x = 3
+scene.add(sphere)
 
-const torusKnot: THREE.Mesh = new THREE.Mesh(torusKnotGeometry, material);
-torusKnot.position.x = -5;
-scene.add(torusKnot);
+const icosahedron = new THREE.Mesh(icosahedronGeometry, material)
+icosahedron.position.x = 0
+scene.add(icosahedron)
 
-window.addEventListener("resize", () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  render();
-});
+const plane = new THREE.Mesh(planeGeometry, material)
+plane.position.x = -2
+scene.add(plane)
 
-const stats: Stats = Stats();
-document.body.appendChild(stats.dom);
+const torusKnot = new THREE.Mesh(torusKnotGeometry, material)
+torusKnot.position.x = -5
+scene.add(torusKnot)
 
-// const options = {
-//     side: {
-//         "FrontSide": THREE.FrontSide,
-//         "BackSide": THREE.BackSide,
-//         "DoubleSide": THREE.DoubleSide,
-//     }
-// }
-
-const gui: GUI = new GUI();
-const materialFolder: GUI = gui.addFolder("THREE.Material");
-// materialFolder.add(material, "transparent").onChange(() => material.needsUpdate = true);
-// materialFolder.add(material, "opacity", 0, 1, .01);
-// materialFolder.add(material, "depthTest");
-// materialFolder.add(material, "depthWrite");
-// materialFolder.add(material, "alphaTest", 0, 1, .01).onChange(() => updateMaterial());
-// materialFolder.add(material, "visible");
-// materialFolder.add(material, "side", options.side).onChange(() => updateMaterial());
-materialFolder.open();
-
-function updateMaterial(): void {
-    material.side = Number(material.side);
-    material.needsUpdate = true;
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
 }
 
-function animate(): void {
-  //   controls.update();
-  render();
-  stats.update();
-  requestAnimationFrame(animate);
+const stats = Stats()
+document.body.appendChild(stats.dom)
+
+const options = {
+    side: {
+        FrontSide: THREE.FrontSide,
+        BackSide: THREE.BackSide,
+        DoubleSide: THREE.DoubleSide,
+    },
+    combine: {
+        MultiplyOperation: THREE.MultiplyOperation,
+        MixOperation: THREE.MixOperation,
+        AddOperation: THREE.AddOperation,
+    },
 }
 
-function render(): void {
-  renderer.render(scene, camera);
+const gui = new GUI()
+const materialFolder = gui.addFolder('THREE.Material')
+materialFolder.add(material, 'transparent').onChange(() => material.needsUpdate = true)
+materialFolder.add(material, 'opacity', 0, 1, 0.01)
+materialFolder.add(material, 'depthTest')
+materialFolder.add(material, 'depthWrite')
+materialFolder
+    .add(material, 'alphaTest', 0, 1, 0.01)
+    .onChange(() => updateMaterial())
+materialFolder.add(material, 'visible')
+materialFolder
+    .add(material, 'side', options.side)
+    .onChange(() => updateMaterial())
+materialFolder.open()
+
+const data = {
+    color: material.color.getHex(),
 }
 
-animate();
-// render();
+const meshBasicMaterialFolder = gui.addFolder('THREE.MeshBasicMaterial')
+meshBasicMaterialFolder.addColor(data, 'color').onChange(() => { material.color.setHex(Number(data.color.toString().replace('#', '0x'))) })
+meshBasicMaterialFolder.add(material, 'wireframe')
+// meshBasicMaterialFolder.add(material, 'wireframeLinewidth', 0, 10)
+meshBasicMaterialFolder.add(material, 'combine', options.combine).onChange(() => updateMaterial())
+meshBasicMaterialFolder.add(material, 'reflectivity', 0, 1)
+meshBasicMaterialFolder.add(material, 'refractionRatio', 0, 1)
+meshBasicMaterialFolder.open()
+
+function updateMaterial() {
+    material.side = Number(material.side)
+    material.combine = Number(material.combine)
+    material.needsUpdate = true
+}
+
+function animate() {
+    requestAnimationFrame(animate)
+
+    render()
+
+    stats.update()
+}
+
+function render() {
+    renderer.render(scene, camera)
+}
+
+animate()
