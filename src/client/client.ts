3011df8e0ed3bee28d08b7ff9ca473bb122dab84
Blessing <bntoko@gmail.com>
2023-02-04 13:34:19 +0200

    normalMap

--- src/client/client.ts
+++ src/client/client.ts
@@ -7,7 +7,7 @@ const scene = new THREE.Scene()
 scene.add(new THREE.AxesHelper(5))
 
 const light = new THREE.PointLight(0xffffff, 2)
-light.position.set(0, 5, 10)
+light.position.set(0, 2, 5)
 scene.add(light)
 
 const camera = new THREE.PerspectiveCamera(
@@ -16,41 +16,29 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
-camera.position.z = 3
+camera.position.z = 1
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
-controls.screenSpacePanning = true //so that panning up and down doesn't zoom in/out
-//controls.addEventListener('change', render)
+controls.enableDamping = true
 
 const planeGeometry = new THREE.PlaneGeometry(3.6, 1.8)
 
-const material = new THREE.MeshPhysicalMaterial({})
+const material = new THREE.MeshPhongMaterial()
 
-//const texture = new THREE.TextureLoader().load("img/grid.png")
 const texture = new THREE.TextureLoader().load('img/worldColour.5400x2700.jpg')
 material.map = texture
-// const envTexture = new THREE.CubeTextureLoader().load(["img/px_50.png", "img/nx_50.png", "img/py_50.png", "img/ny_50.png", "img/pz_50.png", "img/nz_50.png"])
-const envTexture = new THREE.CubeTextureLoader().load([
-    'img/px_eso0932a.jpg',
-    'img/nx_eso0932a.jpg',
-    'img/py_eso0932a.jpg',
-    'img/ny_eso0932a.jpg',
-    'img/pz_eso0932a.jpg',
-    'img/nz_eso0932a.jpg',
-])
-envTexture.mapping = THREE.CubeReflectionMapping
-material.envMap = envTexture
 
-//const specularTexture = new THREE.TextureLoader().load("img/grayscale-test.png")
-const specularTexture = new THREE.TextureLoader().load('img/earthSpecular.jpg')
-material.roughnessMap = specularTexture
-material.metalnessMap = specularTexture
+const normalTexture = new THREE.TextureLoader().load(
+    'img/earth_normalmap_8192x4096.jpg'
+)
+material.normalMap = normalTexture
+material.normalScale.set(2, 2)
 
-const plane: THREE.Mesh = new THREE.Mesh(planeGeometry, material)
+const plane = new THREE.Mesh(planeGeometry, material)
 scene.add(plane)
 
 window.addEventListener('resize', onWindowResize, false)
@@ -64,65 +52,15 @@ function onWindowResize() {
 const stats = Stats()
 document.body.appendChild(stats.dom)
 
-const options = {
-    side: {
-        FrontSide: THREE.FrontSide,
-        BackSide: THREE.BackSide,
-        DoubleSide: THREE.DoubleSide,
-    },
-}
 const gui = new GUI()
-
-const materialFolder = gui.addFolder('THREE.Material')
-materialFolder.add(material, 'transparent')
-materialFolder.add(material, 'opacity', 0, 1, 0.01)
-materialFolder.add(material, 'depthTest')
-materialFolder.add(material, 'depthWrite')
-materialFolder
-    .add(material, 'alphaTest', 0, 1, 0.01)
-    .onChange(() => updateMaterial())
-materialFolder.add(material, 'visible')
-materialFolder
-    .add(material, 'side', options.side)
-    .onChange(() => updateMaterial())
-//materialFolder.open()
-
-const data = {
-    color: material.color.getHex(),
-    emissive: material.emissive.getHex(),
-}
-
-const meshPhysicalMaterialFolder = gui.addFolder(
-    'THREE.meshPhysicalMaterialFolder'
-)
-
-meshPhysicalMaterialFolder.addColor(data, 'color').onChange(() => {
-    material.color.setHex(Number(data.color.toString().replace('#', '0x')))
-})
-meshPhysicalMaterialFolder.addColor(data, 'emissive').onChange(() => {
-    material.emissive.setHex(
-        Number(data.emissive.toString().replace('#', '0x'))
-    )
-})
-meshPhysicalMaterialFolder.add(material, 'wireframe')
-meshPhysicalMaterialFolder
-    .add(material, 'flatShading')
-    .onChange(() => updateMaterial())
-meshPhysicalMaterialFolder.add(material, 'reflectivity', 0, 1)
-meshPhysicalMaterialFolder.add(material, 'envMapIntensity', 0, 1)
-meshPhysicalMaterialFolder.add(material, 'roughness', 0, 1)
-meshPhysicalMaterialFolder.add(material, 'metalness', 0, 1)
-meshPhysicalMaterialFolder.add(material, 'clearcoat', 0, 1, 0.01)
-meshPhysicalMaterialFolder.add(material, 'clearcoatRoughness', 0, 1, 0.01)
-meshPhysicalMaterialFolder.open()
-
-function updateMaterial() {
-    material.side = Number(material.side)
-    material.needsUpdate = true
-}
+gui.add(material.normalScale, 'x', 0, 10, 0.01)
+gui.add(material.normalScale, 'y', 0, 10, 0.01)
+gui.add(light.position, 'x', -20, 20).name('Light Pos X')
 
 function animate() {
     requestAnimationFrame(animate)
+
+    controls.update()
 
     render()
 
