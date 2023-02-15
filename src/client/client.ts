cfbd6423ffe5786b30c21802ca8a1864b1e4774b
Blessing <bntoko@gmail.com>
2023-02-15 14:57:30 +0200

    pointer_lock_controls

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,53 +1,101 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import Stats from 'three/examples/jsm/libs/stats.module'
-import { GUI } from 'dat.gui'
+import * as THREE from "three";
+import { PointerLockControls } from "three/examples/jsm/controls/PointerLockControls";
+import Stats from "three/examples/jsm/libs/stats.module";
 
-const scene = new THREE.Scene()
-scene.add(new THREE.AxesHelper(5))
-
-const light = new THREE.PointLight(0xffffff, 2)
-light.position.set(0, 5, 10)
-scene.add(light)
+const scene = new THREE.Scene();
+scene.add(new THREE.AxesHelper(5));
 
 const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-)
-camera.position.z = 3
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.y = 1;
+camera.position.z = 2;
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.screenSpacePanning = true //so that panning up and down doesn't zoom in/out
-//controls.addEventListener('change', render)
+const menuPanel = document.getElementById("menuPanel") as HTMLDivElement;
+const startBtn = document.getElementById("startButton") as HTMLInputElement;
+startBtn.addEventListener(
+  "click",
+  function () {
+    controls.lock();
+  },
+  false
+);
 
-const planeGeometry = new THREE.PlaneGeometry(3.6, 1.8, 360, 180)
+const controls = new PointerLockControls(camera, renderer.domElement);
+// controls.addEventListener('change', () => console.log("Controls Change"));
+controls.addEventListener('lock', () => menuPanel.style.display = 'none');
+controls.addEventListener('unlock', () => menuPanel.style.display = 'block');
 
-const material = new THREE.MeshPhongMaterial()
+const planeGeometry = new THREE.PlaneGeometry(100, 100, 50, 50);
+const material = new THREE.MeshBasicMaterial({
+  color: 0x00ff00,
+  wireframe: true,
+});
+const plane = new THREE.Mesh(planeGeometry, material);
+plane.rotateX(-Math.PI / 2);
+scene.add(plane);
 
-//const texture = new THREE.TextureLoader().load("img/grid.png")
-const texture = new THREE.TextureLoader().load('img/worldColour.5400x2700.jpg')
-material.map = texture
-// const envTexture = new THREE.CubeTextureLoader().load(["img/px_50.png", "img/nx_50.png", "img/py_50.png", "img/ny_50.png", "img/pz_50.png", "img/nz_50.png"])
-// const envTexture = new THREE.CubeTextureLoader().load(["img/px_eso0932a.jpg", "img/nx_eso0932a.jpg", "img/py_eso0932a.jpg", "img/ny_eso0932a.jpg", "img/pz_eso0932a.jpg", "img/nz_eso0932a.jpg"])
-// envTexture.mapping = THREE.CubeReflectionMapping
-// material.envMap = envTexture
+const cubes: THREE.Mesh[] = [];
+for (let i = 0; i < 100; i++) {
+  const geo = new THREE.BoxGeometry(
+    Math.random() * 4,
+    Math.random() * 16,
+    Math.random() * 4
+  );
+  const mat = new THREE.MeshBasicMaterial({ wireframe: true });
+  switch (i % 3) {
+    case 0:
+      mat.color = new THREE.Color(0xff0000);
+      break;
+    case 1:
+      mat.color = new THREE.Color(0xffff00);
+      break;
+    case 2:
+      mat.color = new THREE.Color(0x0000ff);
+      break;
+    default:
+      break;
+  }
+  const cube = new THREE.Mesh(geo, mat);
 
-//const specularTexture = new THREE.TextureLoader().load("img/earthSpecular.jpg")
-// material.specularMap = specularTexture
+  cubes.push(cube);
+}
 
-const displacementMap = new THREE.TextureLoader().load(
-    'img/gebco_bathy.5400x2700_8bit.jpg'
-)
-material.displacementMap = displacementMap
+cubes.forEach((cube) => {
+  cube.position.x = Math.random() * 100 - 50;
+  cube.position.z = Math.random() * 100 - 50;
+  cube.geometry.computeBoundingBox();
+  cube.position.y =
+    ((cube.geometry.boundingBox as THREE.Box3).max.y -
+      (cube.geometry.boundingBox as THREE.Box3).min.y) /
+    2;
+  scene.add(cube);
+});
 
-const plane: THREE.Mesh = new THREE.Mesh(planeGeometry, material)
-scene.add(plane)
+const onKeyDown = function(evt: KeyboardEvent) {
+    switch(evt.code) {
+        case 'KeyW':
+            controls.moveForward(.25);
+            break;
+        case 'KeyA':
+            controls.moveRight(-.25);
+            break;
+        case 'KeyS':
+            controls.moveForward(-.25);
+            break;
+        case 'KeyD':
+            controls.moveRight(.25);
+            break;
+    }
+}
+document.addEventListener('keydown', onKeyDown, false);
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -60,105 +108,10 @@ function onWindowResize() {
 const stats = Stats()
 document.body.appendChild(stats.dom)
 
-const options = {
-    side: {
-        FrontSide: THREE.FrontSide,
-        BackSide: THREE.BackSide,
-        DoubleSide: THREE.DoubleSide,
-    },
-}
-const gui = new GUI()
-
-const materialFolder = gui.addFolder('THREE.Material')
-materialFolder.add(material, 'transparent')
-materialFolder.add(material, 'opacity', 0, 1, 0.01)
-materialFolder.add(material, 'depthTest')
-materialFolder.add(material, 'depthWrite')
-materialFolder
-    .add(material, 'alphaTest', 0, 1, 0.01)
-    .onChange(() => updateMaterial())
-materialFolder.add(material, 'visible')
-materialFolder
-    .add(material, 'side', options.side)
-    .onChange(() => updateMaterial())
-//materialFolder.open()
-
-const data = {
-    color: material.color.getHex(),
-    emissive: material.emissive.getHex(),
-    specular: material.specular.getHex(),
-}
-
-const meshPhongMaterialFolder = gui.addFolder('THREE.meshPhongMaterialFolder')
-
-meshPhongMaterialFolder.addColor(data, 'color').onChange(() => {
-    material.color.setHex(Number(data.color.toString().replace('#', '0x')))
-})
-meshPhongMaterialFolder.addColor(data, 'emissive').onChange(() => {
-    material.emissive.setHex(
-        Number(data.emissive.toString().replace('#', '0x'))
-    )
-})
-meshPhongMaterialFolder.addColor(data, 'specular').onChange(() => {
-    material.specular.setHex(
-        Number(data.specular.toString().replace('#', '0x'))
-    )
-})
-meshPhongMaterialFolder.add(material, 'shininess', 0, 1024)
-meshPhongMaterialFolder.add(material, 'wireframe')
-meshPhongMaterialFolder
-    .add(material, 'flatShading')
-    .onChange(() => updateMaterial())
-meshPhongMaterialFolder.add(material, 'reflectivity', 0, 1)
-meshPhongMaterialFolder.add(material, 'refractionRatio', 0, 1)
-meshPhongMaterialFolder.add(material, 'displacementScale', 0, 1, 0.01)
-meshPhongMaterialFolder.add(material, 'displacementBias', -1, 1, 0.01)
-meshPhongMaterialFolder.open()
-
-function updateMaterial() {
-    material.side = Number(material.side)
-    material.needsUpdate = true
-}
-
-const planeData = {
-    width: 3.6,
-    height: 1.8,
-    widthSegments: 360,
-    heightSegments: 180,
-}
-
-const planePropertiesFolder = gui.addFolder('PlaneGeometry')
-//planePropertiesFolder.add(planeData, 'width', 1, 30).onChange(regeneratePlaneGeometry)
-//planePropertiesFolder.add(planeData, 'height', 1, 30).onChange(regeneratePlaneGeometry)
-planePropertiesFolder
-    .add(planeData, 'widthSegments', 1, 360)
-    .onChange(regeneratePlaneGeometry)
-planePropertiesFolder
-    .add(planeData, 'heightSegments', 1, 180)
-    .onChange(regeneratePlaneGeometry)
-planePropertiesFolder.open()
-
-function regeneratePlaneGeometry() {
-    const newGeometry = new THREE.PlaneGeometry(
-        planeData.width,
-        planeData.height,
-        planeData.widthSegments,
-        planeData.heightSegments
-    )
-    plane.geometry.dispose()
-    plane.geometry = newGeometry
-}
-
-const textureFolder = gui.addFolder("Texture")
-textureFolder.add(texture.repeat, 'x', 0.1, 1, 0.1)
-textureFolder.add(texture.repeat, 'y', 0.1, 1, 0.1)
-textureFolder.add(texture.center, 'x', 0, 1, 0.001)
-textureFolder.add(texture.center, 'y', 0, 1, 0.001)
-
-textureFolder.open()
-
 function animate() {
     requestAnimationFrame(animate)
+
+    //controls.update()
 
     render()
 
